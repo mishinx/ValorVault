2b1f61075c45f8097955d864bed62b9fa16b8172
Taras Hoshko <tarikhoshko@gmail.com>
2024-04-24 13:46:57 +0300

    Update models

--- ValorVault/Models/Administrator.cs
+++ ValorVault/Models/Administrator.cs
@@ -1,7 +1,11 @@
-﻿namespace ValorVault.Models
-{
-    public partial class Administrator : UserBase
-    {
-        public int admin_id { get; set; }
-    }
-}
+﻿//using Microsoft.AspNetCore.Identity;
+
+//namespace ValorVault.Models
+//{
+//    public partial class Administrator : IdentityUser<int>
+//    {
+//        public int AdminId { get; set; }
+//        public string email { get; set; }
+//        public string user_password { get; set; }
+//    }
+//}
---
+++ ValorVault/Services/PasswordHasher.cs
@@ -0,0 +1,15 @@
+﻿namespace ValorVault.Services
+{
+    public class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool VerifyPassword(string hashedPassword, string candidatePassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(candidatePassword, hashedPassword);
+        }
+    }
+}
